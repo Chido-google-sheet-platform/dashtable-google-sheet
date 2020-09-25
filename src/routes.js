8c2e22defd50a3b61fec1d694275a5a34e6ad469
Chido-google-sheet-platform <69030701+Chido-google-sheet-platform@users.noreply.github.com>
2020-09-25 13:35:03 -0700

    merge

--- src/routes.js
+++ src/routes.js
@@ -7,51 +7,11 @@ import { DefaultLayout } from "./layouts";
 // Route Views
 import BlogOverview from "./views/BlogOverview";
 import UserProfileLite from "./views/UserProfileLite";
-import AddNewPost from "./views/AddNewPost";
-import Errors from "./views/Errors";
-import ComponentsOverview from "./views/ComponentsOverview";
-import Tables from "./views/Tables";
-import BlogPosts from "./views/BlogPosts";
-import pivot from "./views/Test/PivotTable";
-
-export default [
-  {
-    path: "/",
-    exact: true,
-    layout: DefaultLayout,
-    component: () => <Redirect to="/blog-overview" />
-  },
-  {
-    path: "/blog-overview",
-    layout: DefaultLayout,
-    component: BlogOverview
-  },
-  {
-    path: "/user-profile-lite",
-    layout: DefaultLayout,
+imp
     component: UserProfileLite
   },
   {
-    path: "/add-new-post",
-    layout: DefaultLayout,
-    component: AddNewPost
-  },
-  {
-    path: "/errors",
-    layout: DefaultLayout,
-    component: Errors
-  },
-  {
-    path: "/components-overview",
-    layout: DefaultLayout,
-    component: ComponentsOverview
-  },
-  {
-    path: "/tables",
-    layout: DefaultLayout,
-    component: Tables
-  },
-  {
+    path
     path: "/blog-posts",
     layout: DefaultLayout,
     component: BlogPosts
--- src/withTracker.js
+++ src/withTracker.js
@@ -3,28 +3,6 @@ import GoogleAnalytics from "react-ga";
 
 GoogleAnalytics.initialize(process.env.REACT_APP_GAID || "UA-115105611-2");
 
-const withTracker = (WrappedComponent, options = {}) => {
-  const trackPage = page => {
-    if (process.env.NODE_ENV !== "production") {
-      return;
-    }
-
-    GoogleAnalytics.set({
-      page,
-      ...options
-    });
-    GoogleAnalytics.pageview(page);
-  };
-
-  const BASENAME = process.env.REACT_APP_BASENAME || "";
-
-  // eslint-disable-next-line
-  const HOC = class extends Component {
-    componentDidMount() {
-      // eslint-disable-next-line
-      const page = this.props.location.pathname + this.props.location.search;
-      trackPage(`${BASENAME}${page}`);
-    }
 
     componentDidUpdate(prevProps) {
       const currentPage =
